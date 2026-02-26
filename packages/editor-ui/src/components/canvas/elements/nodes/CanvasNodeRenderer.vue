78d5d550d87418fd7e570c6c01a1113588f9f785
reseteo152-stack <reseteo152@gmail.com>
2026-02-26 23:25:55 +0000

    Initialize release/1.50.1

---
+++ packages/editor-ui/src/components/canvas/elements/nodes/CanvasNodeRenderer.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+import { h, inject } from 'vue';
+import CanvasNodeDefault from '@/components/canvas/elements/nodes/render-types/CanvasNodeDefault.vue';
+import { CanvasNodeKey } from '@/constants';
+
+const node = inject(CanvasNodeKey);
+
+const slots = defineSlots<{
+	default?: () => unknown;
+}>();
+
+const Render = () => {
+	let Component;
+	switch (node?.data.value.render.type) {
+		// @TODO Add support for sticky notes here
+		default:
+			Component = CanvasNodeDefault;
+	}
+
+	return h(Component, slots.default);
+};
+</script>
+
+<template>
+	<Render />
+</template>
