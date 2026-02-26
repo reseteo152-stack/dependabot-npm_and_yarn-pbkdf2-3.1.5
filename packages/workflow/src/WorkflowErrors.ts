30dd8cd8f1ff2fa83372a0e8ed089affd98b9f1a
reseteo152-stack <reseteo152@gmail.com>
2026-02-26 12:53:48 -0800

    Initialize verify-that-eslint-diff-still-works-on-ci

---
+++ packages/workflow/src/WorkflowErrors.ts
@@ -0,0 +1,17 @@
+import { INode } from './Interfaces';
+
+/**
+ * Class for instantiating an operational error, e.g. a timeout error.
+ */
+export class WorkflowOperationError extends Error {
+	node: INode | undefined;
+
+	timestamp: number;
+
+	constructor(message: string, node?: INode) {
+		super(message);
+		this.name = this.constructor.name;
+		this.node = node;
+		this.timestamp = Date.now();
+	}
+}
