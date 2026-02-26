4cf92586c8bec32923acdc9059e8ca45b63155bd
reseteo152-stack <reseteo152@gmail.com>
2026-02-26 12:55:10 -0800

    Initialize n8n-4663-reduce-memory-consumption-and-crashes

---
+++ packages/editor-ui/src/App.vue
@@ -0,0 +1,280 @@
+<template>
+	<div :class="[$style.app, 'root-container']">
+		<LoadingView v-if="loading" />
+		<div
+			v-else
+			id="app"
+			:class="{
+				[$style.container]: true,
+				[$style.sidebarCollapsed]: sidebarMenuCollapsed
+			}"
+		>
+			<div id="header" :class="$style['header']">
+				<router-view name="header"></router-view>
+			</div>
+			<div id="sidebar" :class="$style['sidebar']">
+				<router-view name="sidebar"></router-view>
+			</div>
+			<div id="content" :class="$style['content']">
+				<router-view />
+			</div>
+			<Modals />
+			<Telemetry />
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+import Modals from './components/Modals.vue';
+import LoadingView from './views/LoadingView.vue';
+import Telemetry from './components/Telemetry.vue';
+import {HIRING_BANNER, VIEWS} from './constants';
+
+import mixins from 'vue-typed-mixins';
+import {showMessage} from './components/mixins/showMessage';
+import {IUser} from './Interface';
+import {mapGetters} from 'vuex';
+import {userHelpers} from './components/mixins/userHelpers';
+import {loadLanguage} from './plugins/i18n';
+import {restApi} from '@/components/mixins/restApi';
+import {globalLinkActions} from '@/components/mixins/globalLinkActions';
+
+// Schedules the next memory measurement using setTimeout with
+// a randomized interval.
+function scheduleMeasurement() {
+	// Check measurement API is available.
+	if (!window.crossOriginIsolated) {
+		log('performance.measureUserAgentSpecificMemory() is only available in cross-origin-isolated pages ');
+		log('See https://web.dev/coop-coep/ to learn more')
+		return;
+	}
+	if (!performance.measureUserAgentSpecificMemory) {
+		log('performance.measureUserAgentSpecificMemory() is not available in this browser');
+		return;
+	}
+
+	const interval = measurementInterval();
+	log(`Running next memory measurement in ${Math.round(interval / 1000)} seconds`);
+	setTimeout(performMeasurement, interval);
+}
+
+// Computes a random interval in milliseconds such that on
+// average there is one measurement every one minute.
+// In a real page, use a longer interval, e.g. every 5 minutes.
+// See https://bit.ly/3bR0hys for an explanation of the math.
+function measurementInterval() {
+	return 1 * 10 * 1000;
+}
+
+// Invokes the API, records the result, and schedules
+// the next measurement.
+async function performMeasurement() {
+	// 1. Invoke performance.measureUserAgentSpecificMemory().
+	let result;
+	try {
+		log('Starting memory measurement...');
+		result = await performance.measureUserAgentSpecificMemory();
+	} catch (error) {
+		if (error instanceof DOMException && error.name === 'SecurityError') {
+			log('The context is not secure');
+			log('Make sure that cross-origin isolation is enabled');
+			return;
+		}
+		// Rethrow other errors.
+		throw error;
+	}
+	// 2. Record the result.
+	log('Memory usage: ', result);
+	log(`${result.bytes / 1024 / 1024}MB`);
+
+	// 3. Schedule the next measurement.
+	scheduleMeasurement();
+}
+
+// Log to the page and to the DevTools console for object inspection.
+function log(...strings) {
+	console.log(...strings);
+}
+
+export default mixins(
+	showMessage,
+	userHelpers,
+	restApi,
+	globalLinkActions,
+).extend({
+	name: 'App',
+	components: {
+		LoadingView,
+		Telemetry,
+		Modals,
+	},
+	computed: {
+		...mapGetters('settings', ['isHiringBannerEnabled', 'isTemplatesEnabled', 'isTemplatesEndpointReachable', 'isUserManagementEnabled', 'showSetupPage']),
+		...mapGetters('users', ['currentUser']),
+		...mapGetters('ui', ['sidebarMenuCollapsed']),
+		defaultLocale (): string {
+			return this.$store.getters.defaultLocale;
+		},
+	},
+	data() {
+		return {
+			loading: true,
+		};
+	},
+	methods: {
+		async initSettings(): Promise<void> {
+			try {
+				await this.$store.dispatch('settings/getSettings');
+			} catch (e) {
+				this.$showToast({
+					title: this.$locale.baseText('startupError'),
+					message: this.$locale.baseText('startupError.message'),
+					type: 'error',
+					duration: 0,
+				});
+
+				throw e;
+			}
+		},
+		async loginWithCookie(): Promise<void> {
+			try {
+				await this.$store.dispatch('users/loginWithCookie');
+			} catch (e) {}
+		},
+		async initTemplates(): Promise<void> {
+			if (!this.isTemplatesEnabled) {
+				return;
+			}
+
+			try {
+				await this.$store.dispatch('settings/testTemplatesEndpoint');
+			} catch (e) {
+			}
+		},
+		logHiringBanner() {
+			if (this.isHiringBannerEnabled && this.$route.name !== VIEWS.DEMO) {
+				console.log(HIRING_BANNER); // eslint-disable-line no-console
+			}
+		},
+		async initialize(): Promise<void> {
+			await this.initSettings();
+			await Promise.all([this.loginWithCookie(), this.initTemplates()]);
+		},
+		trackPage() {
+			this.$store.commit('ui/setCurrentView', this.$route.name);
+			if (this.$route && this.$route.meta && this.$route.meta.templatesEnabled) {
+				this.$store.commit('templates/setSessionId');
+			}
+			else {
+				this.$store.commit('templates/resetSessionId'); // reset telemetry session id when user leaves template pages
+			}
+
+			this.$telemetry.page(this.$route);
+		},
+		authenticate() {
+			// redirect to setup page. user should be redirected to this only once
+			if (this.isUserManagementEnabled && this.showSetupPage) {
+				if (this.$route.name === VIEWS.SETUP) {
+					return;
+				}
+
+				this.$router.replace({ name: VIEWS.SETUP });
+				return;
+			}
+
+			if (this.canUserAccessCurrentRoute()) {
+				return;
+			}
+
+			// if cannot access page and not logged in, ask to sign in
+			const user = this.currentUser as IUser | null;
+			if (!user) {
+				const redirect =
+					this.$route.query.redirect ||
+					encodeURIComponent(`${window.location.pathname}${window.location.search}`);
+				this.$router.replace({ name: VIEWS.SIGNIN, query: { redirect } });
+				return;
+			}
+
+			// if cannot access page and is logged in, respect signin redirect
+			if (this.$route.name === VIEWS.SIGNIN && typeof this.$route.query.redirect === 'string') {
+				const redirect = decodeURIComponent(this.$route.query.redirect);
+				if (redirect.startsWith('/')) { // protect against phishing
+					this.$router.replace(redirect);
+					return;
+				}
+			}
+
+			// if cannot access page and is logged in
+			this.$router.replace({ name: VIEWS.HOMEPAGE });
+		},
+		redirectIfNecessary() {
+			const redirect = this.$route.meta && typeof this.$route.meta.getRedirect === 'function' && this.$route.meta.getRedirect(this.$store);
+			if (redirect) {
+				this.$router.replace(redirect);
+			}
+		},
+	},
+	async mounted() {
+		await this.initialize();
+		this.logHiringBanner();
+		this.authenticate();
+		this.redirectIfNecessary();
+
+		this.loading = false;
+
+		this.trackPage();
+		this.$externalHooks().run('app.mount');
+
+		if (this.defaultLocale !== 'en') {
+			void this.$store.dispatch('nodeTypes/getNodeTranslationHeaders');
+		}
+
+		scheduleMeasurement();
+	},
+	watch: {
+		$route(route) {
+			this.authenticate();
+			this.redirectIfNecessary();
+
+			this.trackPage();
+		},
+		defaultLocale(newLocale) {
+			loadLanguage(newLocale);
+		},
+	},
+});
+</script>
+
+<style lang="scss" module>
+.app {
+	height: 100vh;
+	overflow: hidden;
+}
+
+.container {
+	display: grid;
+  grid-template-areas:
+    "sidebar header"
+    "sidebar content";
+  grid-auto-columns: fit-content($sidebar-expanded-width) 1fr;
+  grid-template-rows: fit-content($sidebar-width) 1fr;
+}
+
+.content {
+	grid-area: content;
+	overflow: auto;
+	height: 100vh;
+}
+
+.header {
+	grid-area: header;
+	z-index: 999;
+}
+
+.sidebar {
+	grid-area: sidebar;
+	height: 100vh;
+	z-index: 999;
+}
+</style>
