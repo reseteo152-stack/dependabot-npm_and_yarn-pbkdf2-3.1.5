891ef5d2808dfc8226e0d18ef3633cdfb9dbbb93
reseteo152-stack <reseteo152@gmail.com>
2026-02-26 13:13:55 -0800

    Initialize last-ver

---
+++ packages/nodes-base/nodes/Dropcontact/GenericFunction.ts
@@ -0,0 +1,34 @@
+import type { IExecuteFunctions, IHookFunctions } from 'n8n-core';
+
+import type { IDataObject, ILoadOptionsFunctions } from 'n8n-workflow';
+
+import type { OptionsWithUri } from 'request';
+
+/**
+ * Make an authenticated API request to Bubble.
+ */
+export async function dropcontactApiRequest(
+	this: IHookFunctions | IExecuteFunctions | ILoadOptionsFunctions,
+	method: string,
+	endpoint: string,
+	body: IDataObject,
+	qs: IDataObject,
+) {
+	const options: OptionsWithUri = {
+		method,
+		uri: `https://api.dropcontact.io${endpoint}`,
+		qs,
+		body,
+		json: true,
+	};
+
+	if (!Object.keys(body).length) {
+		delete options.body;
+	}
+
+	if (!Object.keys(qs).length) {
+		delete options.qs;
+	}
+
+	return this.helpers.requestWithAuthentication.call(this, 'dropcontactApi', options);
+}
