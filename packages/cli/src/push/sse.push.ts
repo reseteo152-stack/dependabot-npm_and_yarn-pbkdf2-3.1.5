70d532cf3a92ed0aad4777e3b503ed62c12e4dae
reseteo152-stack <reseteo152@gmail.com>
2026-02-26 23:36:06 +0000

    Initialize patch/1.56.1

---
+++ packages/cli/src/push/sse.push.ts
@@ -0,0 +1,38 @@
+import SSEChannel from 'sse-channel';
+import { Service } from 'typedi';
+
+import { Logger } from '@/Logger';
+
+import { AbstractPush } from './abstract.push';
+import type { PushRequest, PushResponse } from './types';
+
+type Connection = { req: PushRequest; res: PushResponse };
+
+@Service()
+export class SSEPush extends AbstractPush<Connection> {
+	readonly channel = new SSEChannel();
+
+	readonly connections: Record<string, Connection> = {};
+
+	constructor(logger: Logger) {
+		super(logger);
+
+		this.channel.on('disconnect', (_, { req }) => {
+			this.remove(req?.query?.pushRef);
+		});
+	}
+
+	add(pushRef: string, connection: Connection) {
+		super.add(pushRef, connection);
+		this.channel.addClient(connection.req, connection.res);
+	}
+
+	protected close({ res }: Connection) {
+		res.end();
+		this.channel.removeClient(res);
+	}
+
+	protected sendToOneConnection(connection: Connection, data: string) {
+		this.channel.send(data, [connection.res]);
+	}
+}
