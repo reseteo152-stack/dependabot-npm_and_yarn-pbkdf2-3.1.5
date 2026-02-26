78ee67f276cfc829a44b2b0a4d99f70b8ff9b858
reseteo152-stack <reseteo152@gmail.com>
2026-02-26 13:42:22 -0800

    Initialize ask-ai-code

---
+++ packages/cli/src/databases/entities/TagEntity.ts
@@ -0,0 +1,39 @@
+import { BeforeInsert, Column, Entity, Index, ManyToMany, OneToMany, PrimaryColumn } from 'typeorm';
+import { IsString, Length } from 'class-validator';
+import type { WorkflowEntity } from './WorkflowEntity';
+import type { WorkflowTagMapping } from './WorkflowTagMapping';
+import { AbstractEntity } from './AbstractEntity';
+import { generateNanoId } from '../utils/generators';
+
+@Entity()
+export class TagEntity extends AbstractEntity {
+	constructor(data?: Partial<TagEntity>) {
+		super();
+		Object.assign(this, data);
+		if (!this.id) {
+			this.id = generateNanoId();
+		}
+	}
+
+	@BeforeInsert()
+	nanoId() {
+		if (!this.id) {
+			this.id = generateNanoId();
+		}
+	}
+
+	@PrimaryColumn('varchar')
+	id: string;
+
+	@Column({ length: 24 })
+	@Index({ unique: true })
+	@IsString({ message: 'Tag name must be of type string.' })
+	@Length(1, 24, { message: 'Tag name must be $constraint1 to $constraint2 characters long.' })
+	name: string;
+
+	@ManyToMany('WorkflowEntity', 'tags')
+	workflows: WorkflowEntity[];
+
+	@OneToMany('WorkflowTagMapping', 'tags')
+	workflowMappings: WorkflowTagMapping[];
+}
