22a5e55d0d84eedd1885ff4d98dc3c20e8faaf85
reseteo152-stack <reseteo152@gmail.com>
2026-02-26 12:30:56 -0800

    Initialize feature/n8n-2701-elementui-update-element-ui-to-support

---
+++ packages/editor-ui/src/router.ts
@@ -0,0 +1,55 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import MainHeader from '@/components/MainHeader/MainHeader.vue';
+import MainSidebar from '@/components/MainSidebar.vue';
+import NodeView from '@/views/NodeView.vue';
+
+Vue.use(Router);
+
+export default new Router({
+	mode: 'history',
+	// @ts-ignore
+	base: window.BASE_PATH === '/%BASE_PATH%/' ? '/' : window.BASE_PATH,
+	routes: [
+		{
+			path: '/execution/:id',
+			name: 'ExecutionById',
+			components: {
+				default: NodeView,
+				header: MainHeader,
+				sidebar: MainSidebar,
+			},
+		},
+		{
+			path: '/workflow',
+			name: 'NodeViewNew',
+			components: {
+				default: NodeView,
+				header: MainHeader,
+				sidebar: MainSidebar,
+			},
+		},
+		{
+			path: '/workflow/:name',
+			name: 'NodeViewExisting',
+			components: {
+				default: NodeView,
+				header: MainHeader,
+				sidebar: MainSidebar,
+			},
+		},
+		{
+			path: '/',
+			redirect: '/workflow',
+		},
+		{
+			path: '/workflows/templates/:id',
+			name: 'WorkflowTemplate',
+			components: {
+				default: NodeView,
+				header: MainHeader,
+				sidebar: MainSidebar,
+			},
+		},
+	],
+});
