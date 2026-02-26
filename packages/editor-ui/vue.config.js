21d90969a419652d53650199853adc29bbf17d9b
reseteo152-stack <reseteo152@gmail.com>
2026-02-26 12:30:24 -0800

    Initialize fix-i18n-executions-list

---
+++ packages/editor-ui/vue.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+	chainWebpack: config => {
+		config.resolve.symlinks(false);
+		// config.plugins.delete("prefetch"); // enable when language package grows
+	},
+	// transpileDependencies: [
+	//   // 'node_modules/quill'
+	//   /\/node_modules\/quill\//
+	// ]
+	pluginOptions: {
+		webpackBundleAnalyzer: {
+			openAnalyzer: false,
+		},
+		i18n: {
+			locale: "en",
+			fallbackLocale: "en",
+			localeDir: "./src/i18n/locales",
+			enableInSFC: false,
+		},
+	},
+	configureWebpack: {
+		devServer: {
+			disableHostCheck: true,
+		},
+	},
+	css: {
+		loaderOptions: {
+			sass: {
+				prependData: `
+					@import "@/n8n-theme-variables.scss";
+				`,
+			},
+		},
+	},
+	publicPath: process.env.VUE_APP_PUBLIC_PATH ? process.env.VUE_APP_PUBLIC_PATH : '/',
+};
