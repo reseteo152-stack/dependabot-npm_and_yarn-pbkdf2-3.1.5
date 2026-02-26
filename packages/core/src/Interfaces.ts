93b0a8d57a9b4a1910f94009ef5a84f88ffdf342
reseteo152-stack <reseteo152@gmail.com>
2026-02-26 14:16:11 -0800

    Initialize fix_xata_zep_motorhead_memory

---
+++ packages/core/src/Interfaces.ts
@@ -0,0 +1,44 @@
+import type {
+	IPollResponse,
+	ITriggerResponse,
+	IWorkflowSettings as IWorkflowSettingsWorkflow,
+	ValidationResult,
+} from 'n8n-workflow';
+
+export interface IProcessMessage {
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	data?: any;
+	type: string;
+}
+
+export interface IResponseError extends Error {
+	statusCode?: number;
+}
+
+export interface IWorkflowSettings extends IWorkflowSettingsWorkflow {
+	errorWorkflow?: string;
+	timezone?: string;
+	saveManualRuns?: boolean;
+}
+
+export interface IWorkflowData {
+	pollResponses?: IPollResponse[];
+	triggerResponses?: ITriggerResponse[];
+}
+
+export namespace n8n {
+	export interface PackageJson {
+		name: string;
+		version: string;
+		n8n?: {
+			credentials?: string[];
+			nodes?: string[];
+		};
+		author?: {
+			name?: string;
+			email?: string;
+		};
+	}
+}
+
+export type ExtendedValidationResult = Partial<ValidationResult> & { fieldName?: string };
