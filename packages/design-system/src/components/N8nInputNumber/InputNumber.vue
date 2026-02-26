a1ccf3bb4e40f01c0d87973b925a899d88614c15
reseteo152-stack <reseteo152@gmail.com>
2026-02-26 14:46:29 -0800

    Initialize refactor/remove_ai_badge

---
+++ packages/design-system/src/components/N8nInputNumber/InputNumber.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import type { InputSize } from '@/types';
+import { ElInputNumber } from 'element-plus';
+
+type InputNumberProps = {
+	size?: InputSize;
+	min?: number;
+	max?: number;
+	step?: number;
+	precision?: number;
+};
+
+defineProps<InputNumberProps>();
+</script>
+
+<template>
+	<ElInputNumber v-bind="{ ...$props, ...$attrs }" />
+</template>
---
+++ packages/workflow/src/Extensions/Extensions.ts
@@ -0,0 +1,39 @@
+export interface ExtensionMap {
+	typeName: string;
+	functions: Record<string, Extension>;
+}
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+export type Extension = Function & { doc?: DocMetadata };
+
+export type NativeDoc = {
+	typeName: string;
+	properties?: Record<string, { doc?: DocMetadata }>;
+	functions: Record<string, { doc?: DocMetadata }>;
+};
+
+export type DocMetadataArgument = {
+	name: string;
+	type?: string;
+	optional?: boolean;
+	variadic?: boolean;
+	description?: string;
+	default?: string;
+};
+export type DocMetadataExample = {
+	example: string;
+	evaluated?: string;
+	description?: string;
+};
+
+export type DocMetadata = {
+	name: string;
+	returnType: string;
+	description?: string;
+	section?: string;
+	hidden?: boolean;
+	aliases?: string[];
+	args?: DocMetadataArgument[];
+	examples?: DocMetadataExample[];
+	docURL?: string;
+};
