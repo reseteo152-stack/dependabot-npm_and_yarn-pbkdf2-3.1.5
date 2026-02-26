29ecb97aabb2fba206cd550020661ab4a5902d26
reseteo152-stack <reseteo152@gmail.com>
2026-02-26 12:27:50 -0800

    Initialize node-netlify

---
+++ packages/editor-ui/src/components/mixins/showMessage.ts
@@ -0,0 +1,60 @@
+import { Notification } from 'element-ui';
+import { ElNotificationOptions } from 'element-ui/types/notification';
+import mixins from 'vue-typed-mixins';
+
+import { externalHooks } from '@/components/mixins/externalHooks';
+
+export const showMessage = mixins(externalHooks).extend({
+	methods: {
+		$showMessage(messageData: ElNotificationOptions) {
+			messageData.dangerouslyUseHTMLString = true;
+			if (messageData.position === undefined) {
+				messageData.position = 'bottom-right';
+			}
+
+			return Notification(messageData);
+		},
+
+		$showError(error: Error, title: string, message?: string) {
+			const messageLine = message ? `${message}<br/>` : '';
+			this.$showMessage({
+				title,
+				message: `
+					${messageLine}
+					<i>${error.message}</i>
+					${this.collapsableDetails(error)}`,
+				type: 'error',
+				duration: 0,
+			});
+
+			this.$externalHooks().run('showMessage.showError', {
+				title,
+				message,
+				errorMessage: error.message,
+			});
+		},
+
+		// @ts-ignore
+		collapsableDetails({ description, node }: Error) {
+			if (!description) return '';
+
+			const errorDescription =
+				description.length > 500
+					? `${description.slice(0, 500)}...`
+					: description;
+
+			return `
+				<br>
+				<br>
+				<details>
+					<summary
+						style="color: #ff6d5a; font-weight: bold; cursor: pointer;"
+					>
+						Show Details
+					</summary>
+					<p>${node.name}: ${errorDescription}</p>
+				</details>
+			`;
+		},
+	},
+});
