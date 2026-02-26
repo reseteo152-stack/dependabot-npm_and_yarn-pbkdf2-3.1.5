043bb7048f9962319b6a18d1c28f6d620791580e
reseteo152-stack <reseteo152@gmail.com>
2026-02-26 14:03:00 -0800

    Initialize refactor-variables-service

---
+++ packages/cli/test/unit/services/orchestration.service.test.ts
@@ -0,0 +1,124 @@
+import Container from 'typedi';
+import config from '@/config';
+import { LoggerProxy } from 'n8n-workflow';
+import { getLogger } from '@/Logger';
+import { OrchestrationService } from '@/services/orchestration.service';
+import type { RedisServiceWorkerResponseObject } from '@/services/redis/RedisServiceCommands';
+import { eventBus } from '@/eventbus';
+import { RedisService } from '@/services/redis.service';
+import { mockInstance } from '../../integration/shared/utils';
+import { handleWorkerResponseMessage } from '../../../src/services/orchestration/handleWorkerResponseMessage';
+import { handleCommandMessage } from '../../../src/services/orchestration/handleCommandMessage';
+
+const os = Container.get(OrchestrationService);
+
+let queueModeId: string;
+
+function setDefaultConfig() {
+	config.set('executions.mode', 'queue');
+}
+
+const workerRestartEventbusResponse: RedisServiceWorkerResponseObject = {
+	senderId: 'test',
+	workerId: 'test',
+	command: 'restartEventBus',
+	payload: {
+		result: 'success',
+	},
+};
+
+describe('Orchestration Service', () => {
+	beforeAll(async () => {
+		mockInstance(RedisService);
+		LoggerProxy.init(getLogger());
+		jest.mock('ioredis', () => {
+			const Redis = require('ioredis-mock');
+			if (typeof Redis === 'object') {
+				// the first mock is an ioredis shim because ioredis-mock depends on it
+				// https://github.com/stipsan/ioredis-mock/blob/master/src/index.js#L101-L111
+				return {
+					Command: { _transformer: { argument: {}, reply: {} } },
+				};
+			}
+			// second mock for our code
+			// eslint-disable-next-line @typescript-eslint/no-explicit-any
+			return function (...args: any) {
+				return new Redis(args);
+			};
+		});
+		jest.mock('../../../src/services/redis/RedisServicePubSubPublisher', () => {
+			return jest.fn().mockImplementation(() => {
+				return {
+					init: jest.fn(),
+					publishToEventLog: jest.fn(),
+					publishToWorkerChannel: jest.fn(),
+					destroy: jest.fn(),
+				};
+			});
+		});
+		jest.mock('../../../src/services/redis/RedisServicePubSubSubscriber', () => {
+			return jest.fn().mockImplementation(() => {
+				return {
+					subscribeToCommandChannel: jest.fn(),
+					destroy: jest.fn(),
+				};
+			});
+		});
+		setDefaultConfig();
+		queueModeId = config.get('redis.queueModeId');
+	});
+
+	afterAll(async () => {
+		jest.mock('../../../src/services/redis/RedisServicePubSubPublisher').restoreAllMocks();
+		jest.mock('../../../src/services/redis/RedisServicePubSubSubscriber').restoreAllMocks();
+		await os.shutdown();
+	});
+
+	test('should initialize', async () => {
+		await os.init();
+		expect(os.redisPublisher).toBeDefined();
+		expect(os.redisSubscriber).toBeDefined();
+		expect(queueModeId).toBeDefined();
+	});
+
+	test('should handle worker responses', async () => {
+		const response = await handleWorkerResponseMessage(
+			JSON.stringify(workerRestartEventbusResponse),
+		);
+		expect(response.command).toEqual('restartEventBus');
+	});
+
+	test('should handle command messages from others', async () => {
+		jest.spyOn(LoggerProxy, 'warn');
+		const responseFalseId = await handleCommandMessage(
+			JSON.stringify({
+				senderId: 'test',
+				command: 'reloadLicense',
+			}),
+		);
+		expect(responseFalseId).toBeDefined();
+		expect(responseFalseId!.command).toEqual('reloadLicense');
+		expect(responseFalseId!.senderId).toEqual('test');
+		expect(LoggerProxy.warn).toHaveBeenCalled();
+		jest.spyOn(LoggerProxy, 'warn').mockRestore();
+	});
+
+	test('should reject command messages from iteslf', async () => {
+		jest.spyOn(eventBus, 'restart');
+		const response = await handleCommandMessage(
+			JSON.stringify({ ...workerRestartEventbusResponse, senderId: queueModeId }),
+		);
+		expect(response).toBeDefined();
+		expect(response!.command).toEqual('restartEventBus');
+		expect(response!.senderId).toEqual(queueModeId);
+		expect(eventBus.restart).not.toHaveBeenCalled();
+		jest.spyOn(eventBus, 'restart').mockRestore();
+	});
+
+	test('should send command messages', async () => {
+		jest.spyOn(os.redisPublisher, 'publishToCommandChannel');
+		await os.getWorkerIds();
+		expect(os.redisPublisher.publishToCommandChannel).toHaveBeenCalled();
+		jest.spyOn(os.redisPublisher, 'publishToCommandChannel').mockRestore();
+	});
+});
