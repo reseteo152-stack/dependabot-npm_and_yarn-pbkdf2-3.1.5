a736183c73e9fdb200b4c078cac2457e2b288615
reseteo152-stack <reseteo152@gmail.com>
2026-02-26 12:29:17 -0800

    Initialize opencell-node

---
+++ packages/editor-ui/src/components/helpers.ts
@@ -0,0 +1,15 @@
+import dateformat from 'dateformat';
+
+const KEYWORDS_TO_FILTER = ['API', 'OAuth1', 'OAuth2'];
+
+export function convertToDisplayDate (epochTime: number) {
+	return dateformat(epochTime, 'yyyy-mm-dd HH:MM:ss');
+}
+
+export function convertToHumanReadableDate (epochTime: number) {
+	return dateformat(epochTime, 'd mmmm, yyyy @ HH:MM Z');
+}
+
+export function getAppNameFromCredType(name: string) {
+	return name.split(' ').filter((word) => !KEYWORDS_TO_FILTER.includes(word)).join(' ');
+}
---
+++ packages/editor-ui/vue.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+	chainWebpack: config => config.resolve.symlinks(false),
+	// transpileDependencies: [
+	//   // 'node_modules/quill'
+	//   /\/node_modules\/quill\//
+	// ]
+	pluginOptions: {
+		webpackBundleAnalyzer: {
+			openAnalyzer: false,
+		},
+	},
+	configureWebpack: {
+		devServer: {
+			disableHostCheck: true,
+		},
+	},
+	css: {
+		loaderOptions: {
+			sass: {
+				prependData: `
+					@import "@/n8n-theme-variables.scss";
+				`,
+			},
+		},
+	},
+	publicPath: process.env.VUE_APP_PUBLIC_PATH ? process.env.VUE_APP_PUBLIC_PATH : '/',
+};
