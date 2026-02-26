d59772751ff9df6c6fd59d9b6b23d1d151df2636
reseteo152-stack <reseteo152@gmail.com>
2026-02-26 13:11:15 -0800

    Initialize google-drive-performance-part2

---
+++ packages/cli/src/databases/entities/SharedWorkflow.ts
@@ -0,0 +1,28 @@
+import { Entity, ManyToOne, PrimaryColumn, RelationId } from 'typeorm';
+import type { WorkflowEntity } from './WorkflowEntity';
+import type { User } from './User';
+import type { Role } from './Role';
+import { AbstractEntity } from './AbstractEntity';
+
+@Entity()
+export class SharedWorkflow extends AbstractEntity {
+	@ManyToOne('Role', 'sharedWorkflows', { nullable: false })
+	role: Role;
+
+	@ManyToOne('User', 'sharedWorkflows', { primary: true })
+	user: User;
+
+	@PrimaryColumn()
+	@RelationId((sharedWorkflow: SharedWorkflow) => sharedWorkflow.user)
+	userId: string;
+
+	@ManyToOne('WorkflowEntity', 'shared', {
+		primary: true,
+		onDelete: 'CASCADE',
+	})
+	workflow: WorkflowEntity;
+
+	@PrimaryColumn()
+	@RelationId((sharedWorkflow: SharedWorkflow) => sharedWorkflow.workflow)
+	workflowId: number;
+}
