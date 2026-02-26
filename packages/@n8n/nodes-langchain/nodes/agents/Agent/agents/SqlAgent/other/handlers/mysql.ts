a1beabaa0d31fe0231dd5da314b4d8f90d8c5e2c
reseteo152-stack <reseteo152@gmail.com>
2026-02-26 14:23:20 -0800

    Initialize gh/8166

---
+++ packages/@n8n/nodes-langchain/nodes/agents/Agent/agents/SqlAgent/other/handlers/mysql.ts
@@ -0,0 +1,29 @@
+import { type IExecuteFunctions } from 'n8n-workflow';
+import { DataSource } from 'typeorm';
+
+export async function getMysqlDataSource(this: IExecuteFunctions): Promise<DataSource> {
+	const credentials = await this.getCredentials('mySql');
+
+	const dataSource = new DataSource({
+		type: 'mysql',
+		host: credentials.host as string,
+		port: credentials.port as number,
+		username: credentials.user as string,
+		password: credentials.password as string,
+		database: credentials.database as string,
+	});
+
+	if (credentials.ssl === true) {
+		dataSource.setOptions({
+			ssl: {
+				rejectUnauthorized: true,
+			},
+		});
+	} else {
+		dataSource.setOptions({
+			ssl: !['disable', undefined].includes(credentials.ssl as string | undefined),
+		});
+	}
+
+	return dataSource;
+}
