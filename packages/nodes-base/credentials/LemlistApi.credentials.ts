df19e509f3008c57fa1e6449e256f2d52a132c59
reseteo152-stack <reseteo152@gmail.com>
2026-02-26 12:48:49 -0800

    Initialize n8n-4207-new-node-id

---
+++ packages/nodes-base/credentials/LemlistApi.credentials.ts
@@ -0,0 +1,15 @@
+import { ICredentialType, INodeProperties } from 'n8n-workflow';
+
+export class LemlistApi implements ICredentialType {
+	name = 'lemlistApi';
+	displayName = 'Lemlist API';
+	documentationUrl = 'lemlist';
+	properties: INodeProperties[] = [
+		{
+			displayName: 'API Key',
+			name: 'apiKey',
+			type: 'string',
+			default: '',
+		},
+	];
+}
