46fd50122c9f95159ffadd494549411129432b05
reseteo152-stack <reseteo152@gmail.com>
2026-02-26 14:38:00 -0800

    Initialize patch-1.34.2

---
+++ packages/cli/src/license/license.controller.ts
@@ -0,0 +1,34 @@
+import { Get, Post, GlobalScope, RestController } from '@/decorators';
+import { LicenseRequest } from '@/requests';
+import { LicenseService } from './license.service';
+
+@RestController('/license')
+export class LicenseController {
+	constructor(private readonly licenseService: LicenseService) {}
+
+	@Get('/')
+	async getLicenseData() {
+		return await this.licenseService.getLicenseData();
+	}
+
+	@Post('/activate')
+	@GlobalScope('license:manage')
+	async activateLicense(req: LicenseRequest.Activate) {
+		const { activationKey } = req.body;
+		await this.licenseService.activateLicense(activationKey);
+		return await this.getTokenAndData();
+	}
+
+	@Post('/renew')
+	@GlobalScope('license:manage')
+	async renewLicense() {
+		await this.licenseService.renewLicense();
+		return await this.getTokenAndData();
+	}
+
+	private async getTokenAndData() {
+		const managementToken = this.licenseService.getManagementJwt();
+		const data = await this.licenseService.getLicenseData();
+		return { ...data, managementToken };
+	}
+}
