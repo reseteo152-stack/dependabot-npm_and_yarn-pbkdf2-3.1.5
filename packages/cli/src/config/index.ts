34137b7d225c0c4bfc12074e69ebbe7ab44bb3c1
reseteo152-stack <reseteo152@gmail.com>
2026-02-26 13:13:56 -0800

    Initialize slack

---
+++ packages/cli/src/config/index.ts
@@ -0,0 +1,81 @@
+import convict from 'convict';
+import dotenv from 'dotenv';
+import { tmpdir } from 'os';
+import { mkdtempSync, readFileSync } from 'fs';
+import { join } from 'path';
+import { LoggerProxy as Logger } from 'n8n-workflow';
+import { schema } from './schema';
+import { inTest, inE2ETests } from '@/constants';
+
+if (inE2ETests) {
+	// Skip loading config from env variables in end-to-end tests
+	process.env = {
+		E2E_TESTS: 'true',
+		N8N_USER_FOLDER: mkdtempSync(join(tmpdir(), 'n8n-e2e-')),
+		N8N_DIAGNOSTICS_ENABLED: 'false',
+		N8N_PUBLIC_API_DISABLED: 'true',
+		EXTERNAL_FRONTEND_HOOKS_URLS: '',
+		N8N_PERSONALIZATION_ENABLED: 'false',
+	};
+} else if (inTest) {
+	process.env.N8N_PUBLIC_API_DISABLED = 'true';
+	process.env.N8N_PUBLIC_API_SWAGGERUI_DISABLED = 'true';
+} else {
+	dotenv.config();
+}
+
+const config = convict(schema);
+
+if (inE2ETests) {
+	config.set('enterprise.features.sharing', true);
+}
+
+// eslint-disable-next-line @typescript-eslint/unbound-method
+config.getEnv = config.get;
+
+// Load overwrites when not in tests
+if (!inE2ETests && !inTest) {
+	// Overwrite default configuration with settings which got defined in
+	// optional configuration files
+	const { N8N_CONFIG_FILES } = process.env;
+	if (N8N_CONFIG_FILES !== undefined) {
+		const configFiles = N8N_CONFIG_FILES.split(',');
+		Logger.debug('Loading config overwrites', configFiles);
+		config.loadFile(configFiles);
+	}
+
+	// Overwrite config from files defined in "_FILE" environment variables
+	const overwrites = Object.entries(process.env).reduce<Record<string, string>>(
+		(acc, [envName, fileName]) => {
+			if (envName.endsWith('_FILE') && fileName) {
+				const key = envName.replace(/_FILE$/, '');
+				// @ts-ignore
+				if (key in config._env) {
+					let value: string;
+					try {
+						value = readFileSync(fileName, 'utf8').trim();
+					} catch (error) {
+						// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+						if (error.code === 'ENOENT') {
+							throw new Error(`The file "${fileName}" could not be found.`);
+						}
+						throw error;
+					}
+					Logger.debug('Loading config overwrite', { fileName });
+					acc[key] = value;
+				}
+			}
+			return acc;
+		},
+		{},
+	);
+	config.load(overwrites);
+}
+
+config.validate({
+	allowed: 'strict',
+});
+
+// eslint-disable-next-line import/no-default-export
+export default config;
+export type Config = typeof config;
