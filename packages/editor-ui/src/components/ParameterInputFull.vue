ba2ba440b624d454fe9038e1b17458356565c632
reseteo152-stack <reseteo152@gmail.com>
2026-02-26 12:30:17 -0800

    Initialize fix-digest-auth

---
+++ packages/editor-ui/src/components/ParameterInputFull.vue
@@ -0,0 +1,66 @@
+<template>
+	<n8n-input-label
+		:label="parameter.displayName"
+		:tooltipText="parameter.description"
+		:showTooltip="focused"
+		:bold="false"
+		size="small"
+	>
+		<parameter-input
+			:parameter="parameter"
+			:value="value"
+			:displayOptions="displayOptions"
+			:path="path"
+			:isReadOnly="isReadOnly"
+			@valueChanged="valueChanged"
+			@focus="focused = true"
+			@blur="focused = false"
+			inputSize="small" />
+	</n8n-input-label>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+import {
+	IUpdateInformation,
+} from '@/Interface';
+
+import ParameterInput from '@/components/ParameterInput.vue';
+
+export default Vue
+	.extend({
+		name: 'ParameterInputFull',
+		components: {
+			ParameterInput,
+		},
+		data() {
+			return {
+				focused: false,
+			};
+		},
+		props: [
+			'displayOptions',
+			'isReadOnly',
+			'parameter',
+			'path',
+			'value',
+		],
+		methods: {
+			getArgument (argumentName: string): string | number | boolean | undefined {
+				if (this.parameter.typeOptions === undefined) {
+					return undefined;
+				}
+
+				if (this.parameter.typeOptions[argumentName] === undefined) {
+					return undefined;
+				}
+
+				return this.parameter.typeOptions[argumentName];
+			},
+			valueChanged (parameterData: IUpdateInformation) {
+				this.$emit('valueChanged', parameterData);
+			},
+		},
+	});
+</script>
