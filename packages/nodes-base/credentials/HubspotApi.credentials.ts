7a6e64cc33d39e35bb233f0d3fd7e99a075ea187
reseteo152-stack <reseteo152@gmail.com>
2026-02-26 12:54:51 -0800

    Initialize hubspot-predefined-credentials

---
+++ packages/nodes-base/credentials/HubspotApi.credentials.ts
@@ -0,0 +1,24 @@
+import { IAuthenticateGeneric, ICredentialType, INodeProperties } from 'n8n-workflow';
+
+export class HubspotApi implements ICredentialType {
+	name = 'hubspotApi';
+	displayName = 'HubSpot API';
+	documentationUrl = 'hubspot';
+	properties: INodeProperties[] = [
+		{
+			displayName: 'API Key',
+			name: 'apiKey',
+			type: 'string',
+			default: '',
+		},
+	];
+
+	authenticate: IAuthenticateGeneric = {
+		type: 'generic',
+		properties: {
+			qs: {
+				hapikey: '={{$credentials.apiKey}}',
+			},
+		},
+	};
+}
