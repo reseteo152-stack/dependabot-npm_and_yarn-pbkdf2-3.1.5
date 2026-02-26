233c2273073237ce36d4efeb0ca4d2b2710b3121
reseteo152-stack <reseteo152@gmail.com>
2026-02-26 13:34:34 -0800

    Initialize log-QueryFailedError

---
+++ packages/editor-ui/src/components/Logo.vue
@@ -0,0 +1,24 @@
+<template>
+	<img :src="basePath + 'n8n-logo-expanded.svg'" :class="$style.img" alt="n8n.io" />
+</template>
+
+<script lang="ts">
+import { useRootStore } from '@/stores/n8nRootStore';
+import { mapStores } from 'pinia';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+	computed: {
+		...mapStores(useRootStore),
+		basePath(): string {
+			return this.rootStore.baseUrl;
+		},
+	},
+});
+</script>
+
+<style lang="scss" module>
+.img {
+	height: 32px;
+}
+</style>
