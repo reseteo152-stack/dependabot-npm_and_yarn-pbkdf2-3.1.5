bcef7d663130cb18f18f2b41cfecbc5c7526fe22
reseteo152-stack <reseteo152@gmail.com>
2026-02-26 14:45:46 -0800

    Initialize test-variance-for-expression-evaluation-benchmarks

---
+++ packages/workflow/src/NativeMethods/Number.methods.ts
@@ -0,0 +1,48 @@
+import type { NativeDoc } from '@/Extensions/Extensions';
+
+export const numberMethods: NativeDoc = {
+	typeName: 'Number',
+	functions: {
+		toFixed: {
+			doc: {
+				name: 'toFixed',
+				hidden: true,
+				description:
+					'Formats a number using fixed-point notation. `digits` defaults to null if not given.',
+				docURL:
+					'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/toFixed',
+				returnType: 'string',
+				args: [{ name: 'digits?', type: 'number' }],
+			},
+		},
+		toPrecision: {
+			doc: {
+				name: 'toPrecision',
+				hidden: true,
+				description: 'Returns a string representing the number to the specified precision.',
+				docURL:
+					'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/toPrecision',
+				returnType: 'string',
+				args: [{ name: 'precision?', type: 'number' }],
+			},
+		},
+		toString: {
+			doc: {
+				name: 'toString',
+				description: 'Returns a string representing this number value.',
+				docURL:
+					'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/toString',
+				returnType: 'string',
+			},
+		},
+		toLocaleString: {
+			doc: {
+				name: 'toLocaleString',
+				description: 'Returns a string with a language-sensitive representation of this number.',
+				docURL:
+					'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/toLocaleString',
+				returnType: 'string',
+			},
+		},
+	},
+};
