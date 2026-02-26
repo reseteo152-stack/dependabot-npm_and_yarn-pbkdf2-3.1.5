130f77ff861b05b889dcc99088de7c8c5d2021e3
reseteo152-stack <reseteo152@gmail.com>
2026-02-26 13:15:13 -0800

    Initialize mapping-test-other

---
+++ packages/workflow/src/Extensions/index.ts
@@ -0,0 +1,10 @@
+// eslint-disable-next-line import/no-cycle
+export {
+	extend,
+	hasExpressionExtension,
+	hasNativeMethod,
+	extendTransform,
+	EXTENSION_OBJECTS as ExpressionExtensions,
+} from './ExpressionExtension';
+
+export type { DocMetadata } from './Extensions';
