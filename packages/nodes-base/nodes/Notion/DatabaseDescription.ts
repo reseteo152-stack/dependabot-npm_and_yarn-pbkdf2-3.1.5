c3c09b409522ce81cfa23686997a7ab601813523
reseteo152-stack <reseteo152@gmail.com>
2026-02-26 12:29:38 -0800

    Initialize feature/notion-extended

---
+++ packages/nodes-base/nodes/Notion/DatabaseDescription.ts
@@ -0,0 +1,119 @@
+import {
+	INodeProperties,
+} from 'n8n-workflow';
+
+export const databaseOperations = [
+	{
+		displayName: 'Operation',
+		name: 'operation',
+		type: 'options',
+		displayOptions: {
+			show: {
+				resource: [
+					'database',
+				],
+			},
+		},
+		options: [
+			{
+				name: 'Get',
+				value: 'get',
+				description: 'Get a database',
+			},
+			{
+				name: 'Get All',
+				value: 'getAll',
+				description: 'Get all databases',
+			},
+		],
+		default: 'get',
+		description: 'The operation to perform.',
+	},
+] as INodeProperties[];
+
+export const databaseFields = [
+
+	/* -------------------------------------------------------------------------- */
+	/*                                database:get                                */
+	/* -------------------------------------------------------------------------- */
+	{
+		displayName: 'Database Link or ID',
+		name: 'databaseId',
+		type: 'string',
+		default: '',
+		required: true,
+		displayOptions: {
+			show: {
+				resource: [
+					'database',
+				],
+				operation: [
+					'get',
+				],
+			},
+		},
+		description: `The URL from Notion's 'copy link' functionality (or just the ID contained within the URL)`,
+	},
+	/* -------------------------------------------------------------------------- */
+	/*                                database:getAll                             */
+	/* -------------------------------------------------------------------------- */
+	{
+		displayName: 'Return All',
+		name: 'returnAll',
+		type: 'boolean',
+		displayOptions: {
+			show: {
+				resource: [
+					'database',
+				],
+				operation: [
+					'getAll',
+				],
+			},
+		},
+		default: false,
+		description: 'If all results should be returned or only up to a given limit',
+	},
+	{
+		displayName: 'Limit',
+		name: 'limit',
+		type: 'number',
+		displayOptions: {
+			show: {
+				resource: [
+					'database',
+				],
+				operation: [
+					'getAll',
+				],
+				returnAll: [
+					false,
+				],
+			},
+		},
+		typeOptions: {
+			minValue: 1,
+			maxValue: 100,
+		},
+		default: 50,
+		description: 'How many results to return',
+	},
+	{
+		displayName: 'Simplify Output',
+		name: 'simple',
+		type: 'boolean',
+		displayOptions: {
+			show: {
+				resource: [
+					'database',
+				],
+				operation: [
+					'getAll',
+					'get',
+				],
+			},
+		},
+		default: false,
+		description: 'Whether to return a simplified version of the response instead of the raw data',
+	},
+] as INodeProperties[];
