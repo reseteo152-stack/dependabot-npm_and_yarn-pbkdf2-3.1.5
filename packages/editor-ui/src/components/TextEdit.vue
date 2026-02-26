bd151d88e917744dd39ec8c9614b76d02ac6cd46
reseteo152-stack <reseteo152@gmail.com>
2026-02-26 12:30:49 -0800

    Initialize node-sideload

---
+++ packages/editor-ui/src/components/TextEdit.vue
@@ -0,0 +1,60 @@
+<template>
+	<div v-if="dialogVisible">
+		<el-dialog :visible="dialogVisible" append-to-body width="80%" :title="`${$locale.baseText('textEdit.edit')} ${$locale.nodeText().topParameterDisplayName(parameter)}`" :before-close="closeDialog">
+
+			<div class="ignore-key-press">
+				<n8n-input-label :label="$locale.nodeText().topParameterDisplayName(parameter)">
+					<div @keydown.stop @keydown.esc="closeDialog()">
+						<n8n-input v-model="tempValue" type="textarea" ref="inputField" :value="value" :placeholder="$locale.nodeText().placeholder(parameter)" @change="valueChanged" @keydown.stop="noOp" :rows="15" />
+					</div>
+				</n8n-input-label>
+			</div>
+
+		</el-dialog>
+	</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+	name: 'TextEdit',
+	props: [
+		'dialogVisible',
+		'parameter',
+		'value',
+	],
+	data () {
+		return {
+			tempValue: '', // el-input does not seem to work without v-model so add one
+		};
+	},
+	methods: {
+		valueChanged (value: string) {
+			this.$emit('valueChanged', value);
+		},
+
+		closeDialog () {
+			// Handle the close externally as the visible parameter is an external prop
+			// and is so not allowed to be changed here.
+			this.$emit('closeDialog');
+			return false;
+		},
+	},
+	mounted () {
+		this.tempValue = this.value as string;
+	},
+	watch: {
+		dialogVisible () {
+			if (this.dialogVisible === true) {
+				Vue.nextTick(() => {
+					(this.$refs.inputField as HTMLInputElement).focus();
+				});
+			}
+		},
+		value () {
+			this.tempValue = this.value as string;
+		},
+	},
+});
+</script>
