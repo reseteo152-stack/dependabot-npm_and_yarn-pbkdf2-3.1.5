0c991f1088f57612dbd7aca27e664a63e363a031
reseteo152-stack <reseteo152@gmail.com>
2026-02-26 12:45:06 -0800

    Initialize n8n-3589-review-node-param-display-name-wrong-for-dynamic-multi-options

---
+++ packages/nodes-base/credentials/HttpHeaderAuth.credentials.ts
@@ -0,0 +1,36 @@
+import {
+	IAuthenticateHeaderAuth,
+	ICredentialType,
+	INodeProperties,
+} from 'n8n-workflow';
+
+
+export class HttpHeaderAuth implements ICredentialType {
+	name = 'httpHeaderAuth';
+	displayName = 'Header Auth';
+	documentationUrl = 'httpRequest';
+	genericAuth = true;
+	icon = 'node:n8n-nodes-base.httpRequest';
+	properties: INodeProperties[] = [
+		{
+			displayName: 'Name',
+			name: 'name',
+			type: 'string',
+			default: '',
+
+		},
+		{
+			displayName: 'Value',
+			name: 'value',
+			type: 'string',
+			default: '',
+		},
+	];
+	authenticate = {
+		type: 'headerAuth',
+		properties: {
+			name: '={{credentials.name}}',
+			value: '={{credentials.value}}',
+		},
+	} as IAuthenticateHeaderAuth;
+}
