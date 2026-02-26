6834b7f28f6c0238142ac460e313f5623a9ac6a8
reseteo152-stack <reseteo152@gmail.com>
2026-02-26 23:23:14 +0000

    Initialize gtag-setup

---
+++ packages/editor-ui/src/components/Telemetry.vue
@@ -0,0 +1,79 @@
+<template>
+	<span v-show="false" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { mapStores } from 'pinia';
+import { useRootStore } from '@/stores/root.store';
+import { useSettingsStore } from '@/stores/settings.store';
+import { useUsersStore } from '@/stores/users.store';
+import type { ITelemetrySettings } from 'n8n-workflow';
+import { useProjectsStore } from '@/stores/projects.store';
+
+export default defineComponent({
+	name: 'Telemetry',
+	data() {
+		return {
+			isTelemetryInitialized: false,
+		};
+	},
+	computed: {
+		...mapStores(useRootStore, useSettingsStore, useUsersStore, useProjectsStore),
+		currentUserId(): string {
+			return this.usersStore.currentUserId ?? '';
+		},
+		isTelemetryEnabledOnRoute(): boolean {
+			const routeMeta = this.$route.meta as { telemetry?: { disabled?: boolean } } | undefined;
+			return routeMeta?.telemetry ? !routeMeta.telemetry.disabled : true;
+		},
+		telemetry(): ITelemetrySettings {
+			return this.settingsStore.telemetry;
+		},
+		isTelemetryEnabled(): boolean {
+			return !!this.telemetry?.enabled;
+		},
+	},
+	watch: {
+		telemetry() {
+			this.init();
+		},
+		currentUserId(userId) {
+			if (this.isTelemetryEnabled) {
+				this.$telemetry.identify(this.rootStore.instanceId, userId);
+			}
+		},
+		isTelemetryEnabledOnRoute(enabled) {
+			if (enabled) {
+				this.init();
+			}
+		},
+	},
+	mounted() {
+		this.init();
+	},
+	methods: {
+		init() {
+			if (
+				this.isTelemetryInitialized ||
+				!this.isTelemetryEnabledOnRoute ||
+				!this.isTelemetryEnabled
+			)
+				return;
+
+			this.$gtag('Session started', {
+				instanceId: this.rootStore.instanceId,
+			});
+
+			this.$telemetry.init(this.telemetry, {
+				instanceId: this.rootStore.instanceId,
+				userId: this.currentUserId,
+				projectId: this.projectsStore.personalProject?.id,
+				versionCli: this.rootStore.versionCli,
+			});
+
+			this.isTelemetryInitialized = true;
+		},
+	},
+});
+</script>
