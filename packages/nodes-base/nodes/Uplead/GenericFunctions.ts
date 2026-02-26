b045718f6fd1151a7b3a6f2f7e3da8723b4ebe06
reseteo152-stack <reseteo152@gmail.com>
2026-02-26 12:44:12 -0800

    Initialize n8n-3711-node-fix-http-request-endpoint-returning-null

---
+++ packages/nodes-base/nodes/Uplead/GenericFunctions.ts
@@ -0,0 +1,29 @@
+import { OptionsWithUri } from 'request';
+import {
+	IExecuteFunctions,
+	IExecuteSingleFunctions,
+	IHookFunctions,
+	ILoadOptionsFunctions,
+} from 'n8n-core';
+import { IDataObject, NodeApiError, NodeOperationError, } from 'n8n-workflow';
+
+export async function upleadApiRequest(this: IHookFunctions | IExecuteFunctions | IExecuteSingleFunctions | ILoadOptionsFunctions, method: string, resource: string, body: any = {}, qs: IDataObject = {}, uri?: string, option: IDataObject = {}): Promise<any> { // tslint:disable-line:no-any
+	const credentials = await this.getCredentials('upleadApi');
+	let options: OptionsWithUri = {
+		headers: { Authorization: credentials.apiKey },
+		method,
+		qs,
+		body,
+		uri: uri ||`https://api.uplead.com/v2${resource}`,
+		json: true,
+	};
+	options = Object.assign({}, options, option);
+	if (Object.keys(options.body).length === 0) {
+		delete options.body;
+	}
+	try {
+		return await this.helpers.request!(options);
+	} catch (error) {
+		throw new NodeApiError(this.getNode(), error);
+	}
+}
