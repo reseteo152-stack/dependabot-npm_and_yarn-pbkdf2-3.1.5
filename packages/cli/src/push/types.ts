5eb61ffc7cf9ebccfbab1b843a4bebb7d16daad9
reseteo152-stack <reseteo152@gmail.com>
2026-02-26 14:35:08 -0800

    Initialize 1.30.2-pr

---
+++ packages/cli/src/push/types.ts
@@ -0,0 +1,19 @@
+import type { AuthenticatedRequest } from '@/requests';
+import type { User } from '@db/entities/User';
+import type { Response } from 'express';
+import type { WebSocket } from 'ws';
+
+// TODO: move all push related types here
+
+export type PushRequest = AuthenticatedRequest<{}, {}, {}, { sessionId: string }>;
+
+export type SSEPushRequest = PushRequest & { ws: undefined };
+export type WebSocketPushRequest = PushRequest & { ws: WebSocket };
+
+export type PushResponse = Response & { req: PushRequest };
+
+export type OnPushMessageEvent = {
+	sessionId: string;
+	userId: User['id'];
+	msg: unknown;
+};
