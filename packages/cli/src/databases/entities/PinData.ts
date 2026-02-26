e07e8232fa6ff1de02c80de3774851284e2e3b8d
reseteo152-stack <reseteo152@gmail.com>
2026-02-26 12:42:16 -0800

    Initialize n8n-3772-pin-and-unpin-data-endpoints

---
+++ packages/cli/src/databases/entities/PinData.ts
@@ -0,0 +1,13 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import config from '../../../config';
+
+@Entity()
+export class PinData {
+	@PrimaryGeneratedColumn()
+	id: number;
+
+	@Column({
+		type: config.getEnv('database.type') === 'sqlite' ? 'text' : 'json',
+	})
+	data: unknown;
+}
