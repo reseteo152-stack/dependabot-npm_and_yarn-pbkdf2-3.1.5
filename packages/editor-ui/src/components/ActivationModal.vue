60706061105a848316ebc5861569c4a09576c108
reseteo152-stack <reseteo152@gmail.com>
2026-02-26 12:30:40 -0800

    Initialize feat/activator-only-for-triggers

---
+++ packages/editor-ui/src/components/ActivationModal.vue
@@ -0,0 +1,112 @@
+<template>
+	<Modal
+		:name="WORKFLOW_ACTIVE_MODAL_KEY"
+		width="460px"
+		minWidth="350px"
+		:title="$locale.baseText('activationModal.modalTitle')"
+	>
+		<template v-slot:content>
+			<div>
+				<n8n-text>{{triggerContent}}</n8n-text>
+			</div>
+			<div :class="$style.spaced">
+				<n8n-text><n8n-text :bold="true">{{ $locale.baseText('activationModal.messageFirstChunk') }}</n8n-text>{{ $locale.baseText('activationModal.messageSecondChunk') }}<a @click="showExecutionsList">{{ $locale.baseText('activationModal.messageExecutionsLink') }}</a>{{ $locale.baseText('activationModal.messageThirdChunk') }}<a @click="showSettings">{{ $locale.baseText('activationModal.messageSaveLink') }}</a>.</n8n-text>
+			</div>
+		</template>
+
+
+		<template v-slot:footer="{ close }">
+			<div :class="$style.footer">
+				<el-checkbox :value="checked" @change="handleCheckboxChange">{{ $locale.baseText('activationModal.checkboxLabel') }}</el-checkbox>
+				<n8n-button @click="close" label="Got it"/>
+			</div>
+		</template>
+	</Modal>
+</template>
+
+<script lang="ts">
+
+import Vue from 'vue';
+
+import Modal from '@/components/Modal.vue';
+import { WORKFLOW_ACTIVE_MODAL_KEY, EXECUTIONS_MODAL_KEY, WORKFLOW_SETTINGS_MODAL_KEY, LOCAL_STORAGE_ACTIVATION_FLAG } from '../constants';
+import { INodeUi } from '../Interface';
+import { getTriggerNodeServiceName } from './helpers';
+
+export default Vue.extend({
+	name: 'ActivationModal',
+	components: {
+		Modal,
+	},
+	props: [
+		'modalName',
+	],
+	data () {
+		return {
+			WORKFLOW_ACTIVE_MODAL_KEY,
+			checked: false,
+		};
+	},
+	methods: {
+		async showExecutionsList () {
+			this.$store.dispatch('ui/openModal', EXECUTIONS_MODAL_KEY);
+		},
+		async showSettings() {
+			this.$store.dispatch('ui/openModal', WORKFLOW_SETTINGS_MODAL_KEY);
+		},
+		handleCheckboxChange (checkboxValue: boolean) {
+			this.checked = checkboxValue;
+			window.localStorage.setItem(LOCAL_STORAGE_ACTIVATION_FLAG, checkboxValue.toString());
+		},
+	},
+	computed: {
+		triggerContent (): string {
+			const foundTriggers = this.$store.getters.workflowTriggerNodes
+				.filter((node: INodeUi) => {
+					return !node.disabled;
+				})
+				.filter(({ type }: INodeUi) => {
+					return type !== 'n8n-nodes-base.errorTrigger';
+				});
+			// if multiple triggers
+			if (foundTriggers.length > 1) {
+				return 'Your triggers will now fire production executions automatically.';
+			}
+			const trigger = foundTriggers[0];
+			const triggerNodeType = this.$store.getters.nodeType(trigger.type);
+
+			if (triggerNodeType.activationMessage) {
+				return triggerNodeType.activationMessage;
+			}
+			const serviceName = getTriggerNodeServiceName(triggerNodeType.displayName);
+			//check if webhook
+			if (trigger.webhookId) {
+				return `Your workflow will now listen for events from ${serviceName} and trigger executions.`;
+			} else if (triggerNodeType.polling) {
+				//check if a polling trigger
+				return `Your workflow will now regularly check ${serviceName} for events and trigger executions for them.`;
+			} else {
+				// default message
+				return 'Your trigger will now fire production executions automatically.';
+			}
+		},
+	},
+});
+</script>
+
+
+<style lang="scss" module>
+
+.spaced {
+	margin-top: var(--spacing-2xs);
+}
+
+.footer {
+	text-align: right;
+
+	> * {
+		margin-left: var(--spacing-s);
+	}
+}
+
+</style>
