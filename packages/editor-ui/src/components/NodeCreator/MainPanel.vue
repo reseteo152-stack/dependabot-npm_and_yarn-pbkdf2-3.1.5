f2dc0ae25bae45be76152052eeea3efd43f323a7
reseteo152-stack <reseteo152@gmail.com>
2026-02-26 12:54:09 -0800

    Initialize n8n-4847-adding-a-node-from-node-creator-by

---
+++ packages/editor-ui/src/components/NodeCreator/MainPanel.vue
@@ -0,0 +1,134 @@
+<template>
+	<div
+		class="container"
+		ref="mainPanelContainer"
+	>
+		<div class="main-panel">
+			<div class="type-selector" v-if="showNodeCreatorTabs" >
+				<el-tabs stretch :value="selectedType" @input="setType">
+					<el-tab-pane :label="$locale.baseText('nodeCreator.mainPanel.all')" :name="ALL_NODE_FILTER"></el-tab-pane>
+					<el-tab-pane :label="$locale.baseText('nodeCreator.mainPanel.regular')" :name="REGULAR_NODE_FILTER"></el-tab-pane>
+					<el-tab-pane :label="$locale.baseText('nodeCreator.mainPanel.trigger')" :name="TRIGGER_NODE_FILTER"></el-tab-pane>
+				</el-tabs>
+			</div>
+			<trigger-helper-panel
+				v-if="selectedType === TRIGGER_NODE_FILTER"
+				:searchItems="searchItems"
+				@nodeTypeSelected="nodeType => $emit('nodeTypeSelected', nodeType)"
+			/>
+			<categorized-items
+				v-else
+				:searchItems="searchItems"
+				:excludedSubcategories="[OTHER_TRIGGER_NODES_SUBCATEGORY]"
+				@nodeTypeSelected="nodeType => $emit('nodeTypeSelected', nodeType)"
+			/>
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+import { PropType } from 'vue';
+import { externalHooks } from '@/components/mixins/externalHooks';
+import mixins from 'vue-typed-mixins';
+import TriggerHelperPanel from './TriggerHelperPanel.vue';
+import { ALL_NODE_FILTER, REGULAR_NODE_FILTER, TRIGGER_NODE_FILTER, OTHER_TRIGGER_NODES_SUBCATEGORY } from '@/constants';
+import CategorizedItems from './CategorizedItems.vue';
+import SlideTransition from '@/components/transitions/SlideTransition.vue';
+import { INodeCreateElement } from '@/Interface';
+
+export default mixins(externalHooks).extend({
+	name: 'NodeCreateList',
+	components: {
+		TriggerHelperPanel,
+		CategorizedItems,
+		SlideTransition,
+	},
+	props: {
+		searchItems: {
+			type: Array as PropType<INodeCreateElement[] | null>,
+		},
+	},
+	data() {
+		return {
+			REGULAR_NODE_FILTER,
+			TRIGGER_NODE_FILTER,
+			ALL_NODE_FILTER,
+			OTHER_TRIGGER_NODES_SUBCATEGORY,
+		};
+	},
+	watch: {
+		selectedType(newValue, oldValue) {
+			this.$externalHooks().run('nodeCreateList.selectedTypeChanged', {
+				oldValue,
+				newValue,
+			});
+			this.$telemetry.trackNodesPanel('nodeCreateList.selectedTypeChanged', {
+				old_filter: oldValue,
+				new_filter: newValue,
+				workflow_id: this.$store.getters.workflowId,
+			});
+		},
+	},
+	methods: {
+		setType(type: string) {
+			this.$store.commit('ui/setSelectedNodeCreatorType', type);
+		},
+	},
+	computed: {
+		showNodeCreatorTabs(): boolean {
+			return this.$store.getters['ui/showNodeCreatorTabs'];
+		},
+		selectedType(): string {
+			return this.$store.getters['ui/selectedNodeCreatorType'];
+		},
+	},
+	mounted() {
+		this.$externalHooks().run('nodeCreateList.mounted');
+		// Make sure tabs are visible on mount
+		this.$store.commit('ui/setShowNodeCreatorTabs', true);
+	},
+	destroyed() {
+		this.setType(ALL_NODE_FILTER);
+		this.$externalHooks().run('nodeCreateList.destroyed');
+		this.$telemetry.trackNodesPanel('nodeCreateList.destroyed', { workflow_id: this.$store.getters.workflowId });
+	},
+});
+</script>
+
+<style lang="scss" scoped>
+::v-deep .el-tabs__item {
+	padding: 0;
+}
+
+::v-deep .el-tabs__active-bar {
+	height: 1px;
+}
+
+::v-deep .el-tabs__nav-wrap::after {
+	height: 1px;
+}
+
+.container {
+	height: 100%;
+
+	> div {
+		height: 100%;
+	}
+}
+
+.type-selector {
+	text-align: center;
+	background-color: $--node-creator-select-background-color;
+
+	::v-deep .el-tabs > div {
+		margin-bottom: 0;
+
+		.el-tabs__nav {
+			height: 43px;
+		}
+	}
+}
+.main-panel {
+	background-color: var(--color-background-xlight);
+}
+</style>
---
+++ packages/editor-ui/src/components/transitions/SlideTransition.vue
@@ -0,0 +1,49 @@
+<template>
+	<transition
+		name="slide"
+		v-bind="animationClasses"
+	>
+		<slot></slot>
+	</transition>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+	props: {
+		absolute: {
+			type: Boolean,
+			default: false,
+		},
+	},
+	name: 'SlideTransition',
+	computed: {
+		animationClasses() {
+			const absouteClass = this.absolute ? 'absolute' : '';
+			return {
+				'enter-active-class': `slide-enter-active ${absouteClass}`,
+				'leave-active-class': `slide-leave-active ${absouteClass}`,
+			};
+		},
+	},
+});
+</script>
+
+<style lang="scss" scoped>
+.slide-leave-active,
+.slide-enter-active {
+	transition: 0.3s ease;
+
+	&.absolute {
+		position: absolute;
+		left: 0;
+		right: 0;
+	}
+}
+.slide-leave-to,
+.slide-enter {
+	transform: translateX(100%);
+}
+
+</style>
