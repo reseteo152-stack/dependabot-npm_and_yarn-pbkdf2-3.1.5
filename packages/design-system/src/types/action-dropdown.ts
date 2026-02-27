dd9281db26e29f12aab2afd5e3b3006774092cee
reseteo152-stack <reseteo152@gmail.com>
2026-02-27 00:05:13 +0000

    Initialize help-645-resend-invitation-link-does-not-work-anymore-alt

---
+++ packages/design-system/src/types/action-dropdown.ts
@@ -0,0 +1,12 @@
+import type { KeyboardShortcut } from 'n8n-design-system/types/keyboardshortcut';
+
+export interface ActionDropdownItem {
+	id: string;
+	label: string;
+	badge?: string;
+	icon?: string;
+	divided?: boolean;
+	disabled?: boolean;
+	shortcut?: KeyboardShortcut;
+	customClass?: string;
+}
